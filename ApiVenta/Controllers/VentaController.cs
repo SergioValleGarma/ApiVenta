8c3c89823baf4ddd6e760a597e320e99b80f7e95
sergio_valle <sergiovallegarma@gmail.com>
2024-08-11 21:33:55 -0500

    Agregar archivos de proyecto.

---
+++ ApiVenta/Controllers/VentaController.cs
@@ -0,0 +1,22 @@
+﻿using ApiVenta.Modelo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVenta.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class VentaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Venta> Get()
+        {
+            List<Venta> lista = new List<Venta>();
+            for (int i = 0; i < 5; i++)
+            {
+                lista.Add(new Venta { id = i,Nombre="Laptop"+i });
+            }
+            return lista;
+        }
+    }
+}
---
+++ ApiVenta/Modelo/Venta.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiVenta.Modelo
+{
+    public class Venta
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
